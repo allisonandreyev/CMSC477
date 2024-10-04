f91b6056454d8df434d08250bcfc8127db1dbec8
Allison Andreyev <137202319+allisonandreyev@users.noreply.github.com>
2024-10-04 18:14:22 -0400

    Update csvReader.py

--- Project1/csvReader.py
+++ Project1/csvReader.py
@@ -1,20 +1,6 @@
 import csv
 
-arr=[]
-
 def openFile(name):
     with open(name, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            arr.append(row)
-            print(arr)
-
-def djikstra(file):
-    # alg
-
-def bfs(file):
-    # alg
-
-def dfs(file):
-    # alg
-
+        reader = csv.reader(csvfile)
+        return [[int(cell) for cell in row] for row in reader]
